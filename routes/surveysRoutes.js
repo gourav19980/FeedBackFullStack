4f6e1d0618a78258a057b516a4988422b8352b6c
Gourav Kumar Sahu <65903110+gourav19980@users.noreply.github.com>
2020-09-12 00:30:37 +0530

    Added billing and client stuffs

--- routes/surveysRoutes.js
+++ routes/surveysRoutes.js
@@ -1,3 +1,8 @@
+const requireLogin=require('../middleware/requireLogin');
+const requireCredits=require('../middleware/requireCredits');
+
 module.exports = (app) => {
-    
+    app.post('/api/surevys',requireLogin,requireCredits,(req,res)=>{
+
+    });
 };

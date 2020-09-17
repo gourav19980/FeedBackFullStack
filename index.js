ac175cf9a26c1b2546746db35a21596225fa1dc7
Gourav Kumar Sahu <65903110+gourav19980@users.noreply.github.com>
2020-09-17 12:43:04 +0530

    ready to deploy but CORS error Coming

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ if(process.env.NODE_ENV === 'production'){
     //Express will send index.js if no route is there
     const path = require('path');
     app.get('*',(req,res)=>{
+        //res.header("Access-Control-Allow-Origin", "*");
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     });
 }
